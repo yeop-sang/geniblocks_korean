68044c3bba53d04e543bddac244e4b9285800c81
Sam Fentress <sfentress@concord.org>
2016-01-08 09:35:36 -0500

    Add basic GenomeView, with all chromosomes

--- src/code/app.js
+++ src/code/app.js
@@ -3,6 +3,7 @@ export default  {
   OrganismView: require('./components/organism'),
   ChromosomeImageView: require('./components/chromosome-image'),
   ChromosomeView: require('./components/chromosome'),
-  GeneLabelView: require('./components/gene-label')
+  GeneLabelView: require('./components/gene-label'),
+  GenomeView: require('./components/genome')
 }
 
---
+++ src/code/components/genome.js
@@ -0,0 +1,33 @@
+let ChromosomeView = require('./chromosome');
+
+const GenomeView = ({org, alleleChanged}) => {
+  let pairWrappers = [];
+  for (let chromosomeName of org.species.chromosomeNames) {
+    let chrom = org.genetics.genotype.chromosomes[chromosomeName],
+        pairs = [];
+    for (let side in chrom) {
+      pairs.push(
+        <ChromosomeView
+          org={org}
+          chromosomeName={chromosomeName}
+          side={side}
+          labelsOnRight={pairs.length>0}
+          alleleChanged={function(prevAllele, newAllele) {
+            alleleChanged(chromosomeName, side, prevAllele, newAllele);
+          }}/>
+      );
+    }
+    pairWrappers.push(
+      <div>
+        { pairs }
+      </div>
+    );
+  }
+  return (
+    <div className="geniblocks genome">
+      { pairWrappers }
+    </div>
+  );
+}
+
+export default GenomeView;
