f6d8e12390eadb589ce8ef80e74b81846ca635d1
Kirk Swenson <kswenson@concord.org>
2016-04-12 23:49:09 -0700

    Unit test harness

---
+++ test/setup.js
@@ -0,0 +1,29 @@
+/*
+ * Set up jsdom and load BioLogical globally as a precursor for all tests.
+ * See http://stackoverflow.com/a/32999208 and
+ *     https://github.com/facebook/react/issues/5046#issuecomment-146222515
+ * for details of why it's necessary to set up jsdom in a separate file
+ * which is '--require'd on the mocha command line so that the dom is ready
+ * when required by the other tests.
+ *
+ * We take this opportunity to load BioLogica globally for all tests as well.
+ */
+import fs from 'fs';
+import vm from 'vm';
+import jsdom from 'jsdom';
+
+function execScript(path, context) {
+  context = context || {};
+  var data = fs.readFileSync(path, 'utf8');
+  data = data.replace(/window\./g, '');
+  vm.runInNewContext(data, context, path);
+  return context;
+}
+
+if (typeof document === 'undefined') {
+  global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+  global.window = document.defaultView;
+  global.navigator = global.window.navigator;
+
+  execScript('./bower_components/biologica.js/dist/biologica.js', global);
+}
