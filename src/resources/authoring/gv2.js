c253d073b0e66a691b7da41173b9cb96ebe7ba60
Sam Fentress <sfentress@concord.org>
2016-06-14 16:27:11 -0400

    Add simple initial authoring for first two challenges

---
+++ src/resources/authoring/gv2.js
@@ -0,0 +1,28 @@
+window.GV2Authoring = [
+  [
+    {
+      "template": "GenomePlayground",
+      "drakes": [
+        {
+          "alleles": "a:T,b:T,a:w,b:w,a:h,b:h,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+          "sex": 1
+        }
+      ],
+      "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
+    },
+    {
+      "template": "GenomeChallenge",
+      "drakes": [
+        {
+          "alleles": "a:T,b:T,a:h,b:h,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+          "sex": 1
+        },
+        {
+          "alleles": "a:w,a:m,a:fl,a:hl,a:T,b:T,a:h,b:h,a:C,b:C,a:A1,b:A1,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+          "sex": 1
+        }
+      ],
+      "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
+    }
+  ]
+];
