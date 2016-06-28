c29056d88caa1d26c627a292fb5720acf3ee51ab
Sam Fentress <sfentress@concord.org>
2016-06-27 21:35:45 -0400

    Navigate to correct challenge given url route
    Our first three activities can be reached at #/1/1, #/1/2 and #/1/3
    
    Remove `onNavigateNextChallenge` functionality until next commit.

--- src/code/containers/challenge-container.js
+++ src/code/containers/challenge-container.js
@@ -4,6 +4,15 @@ import templates from '../templates';
 import { changeAllele, changeSex, submitDrake, loadAuthoredChallenge } from '../actions';
 
 class ChallengeContainer extends Component {
+  componentWillMount() {
+    this.props.loadAuthoredChallenge(this.props.routeParams.challenge-1);
+  }
+  componentWillReceiveProps(newProps) {
+    if (this.props.routeParams.challenge !== newProps.routeParams.challenge) {
+      this.props.loadAuthoredChallenge(newProps.routeParams.challenge-1);
+    }
+  }
+
   render() {
     const Template = templates[this.props.template];
     return (
@@ -39,7 +48,8 @@ function mapDispatchToProps(dispatch) {
     onChromosomeAlleleChange: (index, chrom, side, prevAllele, newAllele) => dispatch(changeAllele(index, chrom, side, prevAllele, newAllele, true)),
     onSexChange: (index, newSex) => dispatch(changeSex(index, newSex, true)),
     onDrakeSubmission: (targetPhenotype, userPhenotype, correct) => dispatch(submitDrake(targetPhenotype, userPhenotype, correct)),
-    onNavigateNextChallenge: (nextChallenge) => dispatch(loadAuthoredChallenge(nextChallenge)) // hard-coded for the moment
+    onNavigateNextChallenge: () => {},
+    loadAuthoredChallenge: (nextChallenge) => dispatch(loadAuthoredChallenge(nextChallenge)) // hard-coded for the moment
   };
 }
 
