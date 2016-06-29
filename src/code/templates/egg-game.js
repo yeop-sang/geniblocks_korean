d8732ffc81155fdd1ebd0e90685e2415dbe73587
Sam Fentress <sfentress@concord.org>
2016-06-29 10:43:24 -0400

    Render separate mother and father

--- src/code/templates/egg-game.js
+++ src/code/templates/egg-game.js
@@ -5,9 +5,8 @@ import ButtonView from '../components/button';
 export default class EggGame extends Component {
     render() {
       const { drakes, onChromosomeAlleleChange, onChromosomeSelected, onNavigateNextChallenge, hiddenAlleles, challenge } = this.props,
-          drakeDef = drakes[0].alleleString,
-          drakeSex = drakes[0].sex,
-          drake = new BioLogica.Organism(BioLogica.Species.Drake, drakeDef, drakeSex);
+          mother = new BioLogica.Organism(BioLogica.Species.Drake, drakes[0].alleleString, drakes[0].sex),
+          father = new BioLogica.Organism(BioLogica.Species.Drake, drakes[1].alleleString, drakes[1].sex);
 
       const handleAlleleChange = function(chrom, side, prevAllele, newAllele) {
         onChromosomeAlleleChange(0, chrom, side, prevAllele, newAllele);
@@ -23,13 +22,13 @@ export default class EggGame extends Component {
       <div id="egg-game">
         <div className='column'>
           <div>Mother</div>
-            <GenomeView className="drake-genome" org={ drake } onAlleleChange={ handleAlleleChange } onChromosomeSelected={handleChromosomeSelected} editable={false} showAlleles={true} hiddenAlleles= { hiddenAlleles } />
+            <GenomeView className="drake-genome" org={ mother } onAlleleChange={ handleAlleleChange } onChromosomeSelected={handleChromosomeSelected} editable={false} showAlleles={true} hiddenAlleles= { hiddenAlleles } />
             <div>Ovum</div>
             <div className='egg-alleles' />
         </div>
         <div className='column'>
           <div>Father</div>
-            <GenomeView className="drake-genome" org={ drake } onAlleleChange={ handleAlleleChange } onChromosomeSelected={handleChromosomeSelected} editable={false} showAlleles={true} hiddenAlleles= { hiddenAlleles } />
+            <GenomeView className="drake-genome" org={ father } onAlleleChange={ handleAlleleChange } onChromosomeSelected={handleChromosomeSelected} editable={false} showAlleles={true} hiddenAlleles= { hiddenAlleles } />
             <div>Sperm</div>
             <div className='egg-alleles' />
         </div>
@@ -49,6 +48,6 @@ export default class EggGame extends Component {
     challenge: PropTypes.number.isRequired
   }
   static authoredDrakesToDrakeArray = function(authoredChallenge) {
-    return [authoredChallenge.initialDrakes];
+    return [authoredChallenge.mother, authoredChallenge.father];
   }
 }
--- src/resources/authoring/gv2.js
+++ src/resources/authoring/gv2.js
@@ -1,5 +1,5 @@
 window.GV2Authoring = [
-  [    
+  [
     {
       "template": "GenomePlayground",
       "initialDrake": {
@@ -43,13 +43,14 @@ window.GV2Authoring = [
     },
     {
       "template": "EggGame",
-      "initialDrakes":[{
+      "mother":{
         "alleles": "a:w,a:m,b:m,a:fl,a:hl,a:T,b:T,a:h,b:h,a:C,b:C,a:A1,b:A1,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
         "sex": 1
-      },{
+      },
+      "father": {
         "alleles": "a:T,b:T,a:h,b:h,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
         "sex": 0
-      }],
+      },
       "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
     }
   ]
