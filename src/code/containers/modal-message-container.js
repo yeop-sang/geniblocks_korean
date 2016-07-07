1e8aba3f1598d5a7b64b130bb87480b984a9313c
Sam Fentress <sfentress@concord.org>
2016-07-07 16:16:10 -0400

    Update message-container to allow arbitrary button actions

--- src/code/containers/modal-message-container.js
+++ src/code/containers/modal-message-container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import ModalAlert from '../components/modal-alert';
-import { dismissModalDialog, advanceTrial, navigateToNextChallenge } from '../actions';
+import * as actions from '../actions';
 
 const messageProps = {
   MatchDrakeFailure: {
@@ -8,7 +8,7 @@ const messageProps = {
     explanation: "The drake you have created doesn't match the target drake.\nPlease try again.",
     rightButton: {
       label: "Try again",
-      clickFunc: "onDismiss"
+      action: "dismissModalDialog"
     }
   },
   MatchDrakeSuccessLastTrial: {
@@ -16,7 +16,7 @@ const messageProps = {
     explanation: "The drake you have created matches the target drake.",
     rightButton: {
       label: "Next trial",
-      clickFunc: "onAdvanceTrial"
+      action: "advanceTrial"
     }
   },
   ChallengeCompleted: {
@@ -24,7 +24,7 @@ const messageProps = {
     explanation: "You have completed all trials in this challenge.",
     rightButton: {
       label: "Next challenge",
-      clickFunc: "onNavigateToNextChallenge"
+      action: "navigateToNextChallenge"
     },
     challengeAwards: {caseId: 0, challengeId:0, challengeCount: 0, progress: -1}
   }
@@ -32,13 +32,15 @@ const messageProps = {
 
 function mapStateToProps (state) {
   var props;
-  if (state.itsMessage){
+  if (state.message) {
+    props = state.message;
+  } else if (state.itsMessage){
     props = {
       message: "Message from ITS",
       explanation: state.itsMessage.text,
       rightButton: {
         label: "OK",
-        clickFunc: "onDismiss"
+        action: "dismissModalDialog"
       }
     };
   } else {
@@ -64,19 +66,20 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps (dispatch) {
   return {
-    onDismiss: () => dispatch(dismissModalDialog()),
-    onAdvanceTrial: () => dispatch(advanceTrial()),
-    onNavigateToNextChallenge: () => dispatch(navigateToNextChallenge())
+    actionCreator: function (actionName) {
+      return () =>
+        dispatch(actions[actionName]());
+    }
   };
 }
 
 function mergeProps(stateProps, dispatchProps) {
   let props = {...stateProps};
-  if (stateProps.leftButton) {
-    props.onLeftButtonClick = dispatchProps[props.leftButton.clickFunc];
+  if (stateProps.leftButton && props.leftButton.action) {
+    props.onLeftButtonClick = dispatchProps.actionCreator(props.leftButton.action);
   }
-  if (props.rightButton) {
-    props.onRightButtonClick = dispatchProps[props.rightButton.clickFunc];
+  if (props.rightButton && props.rightButton.action) {
+    props.onRightButtonClick = dispatchProps.actionCreator(props.rightButton.action);
   }
   return props;
 }
