ab7db63291b7acc1b4cd30495424674fe1e5cb74
Sam Fentress <sfentress@concord.org>
2016-07-07 16:50:01 -0400

    Add a fake new challenge after egg-game

--- src/resources/authoring/gv2.js
+++ src/resources/authoring/gv2.js
@@ -54,6 +54,14 @@ window.GV2Authoring = [
         "sex": 0
       },
       "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
+    },
+    {
+      "template": "GenomePlayground",
+      "initialDrake": {
+        "alleles": "a:T,b:T,a:w,b:w,a:h,b:h,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "sex": 1
+      },
+      "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
     }
   ]
 ];
