e5e56d1c20edca3c00c2b535cf0e7e205e362580
Sam Fentress <sfentress@concord.org>
2016-07-07 10:08:57 -0400

    Add ability to tighten rows or columns of pen

--- src/code/components/pen.js
+++ src/code/components/pen.js
@@ -4,8 +4,10 @@ import OrganismView from './organism';
 /**
  * @param {number} rows - Option number of rows. If defined, it will be fixed at that. Otherwise, it
  *                        will default to 1 when there are no orgs, and grows as more rows are needed.
+ * @param {number} tightenRows - If given, will shrink the vertical height of the pen by this amount
+ *                        per row, crowding the org images as needed.
  */
-const PenView = ({orgs, idPrefix='organism-', width=400, columns=5, rows, SelectedOrganismView=OrganismView, selectedIndex, onClick}) => {
+const PenView = ({orgs, idPrefix='organism-', width=400, columns=5, rows, tightenRows=0, tightenColumns=0, SelectedOrganismView=OrganismView, selectedIndex, onClick}) => {
 
   function handleClick(id, org) {
     const prefixIndex = id.indexOf(idPrefix),
@@ -13,18 +15,27 @@ const PenView = ({orgs, idPrefix='organism-', width=400, columns=5, rows, Select
     if (onClick) onClick(index, id, org);
   }
 
+  let orgStyle = {
+    margin: `${-tightenRows/2}px ${-tightenColumns/2}px`
+  };
+
   let orgWidth = width/columns,
       orgViews = orgs.map((org, index) => {
         return index === selectedIndex
                 ? <SelectedOrganismView org={org} id={idPrefix + index} index={index} key={index}
-                                    color="#FFFFAA" size={orgWidth} onClick={handleClick}/>
+                                    color="#FFFFAA" size={orgWidth} style={orgStyle} onClick={handleClick}/>
                 : <OrganismView org={org} id={idPrefix + index} index={index} key={index}
-                                width={orgWidth} onClick={handleClick}/>;
+                                width={orgWidth} style={orgStyle} onClick={handleClick} />;
       });
 
   rows = rows || Math.ceil(orgViews.length / columns) || 1;
-  let height = orgWidth * rows,
-      style = { width, height };
+
+  let height = orgWidth * rows;
+
+  width  = width  - (tightenColumns * columns);
+  height = height - (tightenRows * rows);
+
+  let style = { width, height };
 
   return (
     <div className="geniblocks pen" style={style}>
@@ -39,6 +50,8 @@ PenView.propTypes = {
   width: PropTypes.number,
   columns: PropTypes.number,
   rows: PropTypes.number,
+  tightenColumns: PropTypes.number,
+  tightenRows: PropTypes.number,
   SelectedOrganismView: PropTypes.func,
   selectedIndex: PropTypes.number,
   onClick: PropTypes.func
