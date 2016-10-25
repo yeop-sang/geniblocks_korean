e076e230dc3951123d70cfca563ed6d82b0fe80e
Kirk Swenson <kswenson@concord.org>
2016-10-24 18:01:16 -0700

    Add authoring for Case 4 using Egg Game III - trial 1 (horns) - trial 2 (armor) - trial 3 (horns/wings/armor)

--- src/resources/authoring/gv2.js
+++ src/resources/authoring/gv2.js
@@ -89,6 +89,9 @@ window.GV2Authoring = [
     }
   ],
   [
+    /*
+     *  Case 3: Trial 1 - Egg Game III (wings)
+     */
     {
       "template": "EggSortGame",
       "trialGenerator": {
@@ -107,12 +110,15 @@ window.GV2Authoring = [
       },
       "hiddenAlleles": "t,m,h,c,b,fl,hl,a,d,bog,rh",
       "baskets": [
-        { "label": "Males with Wings", "alleles": ["a:W","b:W"], "sex": 0 },
-        { "label": "Males without Wings", "alleles": ["a:w,b:w"], "sex": 0 },
-        { "label": "Females with Wings", "alleles": ["a:W","b:W"], "sex": 1 },
-        { "label": "Females without Wings", "alleles": ["a:w,b:w"], "sex": 1 }
+        { "label": "Males with wings", "alleles": ["a:W","b:W"], "sex": 0 },
+        { "label": "Males without wings", "alleles": ["a:w,b:w"], "sex": 0 },
+        { "label": "Females with wings", "alleles": ["a:W","b:W"], "sex": 1 },
+        { "label": "Females without wings", "alleles": ["a:w,b:w"], "sex": 1 }
       ]
     },
+    /*
+     *  Case 3: Trial 2 - Egg Game III (forelimbs/hindlimbs)
+     */
     {
       "template": "EggSortGame",
       "trialGenerator": {
@@ -133,7 +139,88 @@ window.GV2Authoring = [
       "baskets": [
         { "label": "Drakes with only forelimbs", "alleles": ["a:Fl,a:hl,b:hl", "b:Fl,a:hl,b:hl"] },
         { "label": "Drakes with only hindlimbs", "alleles": ["a:fl,b:fl,a:Hl", "a:fl,b:fl,b:Hl"] },
-        { "label": "Drakes with forelimbs and hindlimbs", "alleles": ["a:Fl,a:Hl","a:Fl,b:Hl","b:Fl,a:Hl","b:Fl,b:Hl"] },
+        { "label": "Drakes with forelimbs and hindlimbs", "alleles": ["a:Fl,a:Hl","a:Fl,b:Hl","b:Fl,a:Hl","b:Fl,b:Hl"] }
+      ]
+    }
+  ],
+  [
+    /*
+     *  Case 4: Trial 1 - Egg Game III (horns)
+     */
+    {
+      "template": "EggSortGame",
+      "trialGenerator": {
+        "type": "randomize-order",
+        "baseDrake": "a:T,b:T,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "drakes": [
+          { alleles: "a:H,b:H", sex: 0 },
+          { alleles: "a:H,b:h", sex: 0 },
+          { alleles: "a:h,b:H", sex: 0 },
+          { alleles: "a:h,b:h", sex: 0 },
+          { alleles: "a:H,b:H", sex: 1 },
+          { alleles: "a:H,b:h", sex: 1 },
+          { alleles: "a:h,b:H", sex: 1 },
+          { alleles: "a:h,b:h", sex: 1 }
+        ],
+      },
+      "hiddenAlleles": "w,t,m,c,b,fl,hl,a,d,bog,rh",
+      "baskets": [
+        { "label": "Males with horns", "alleles": ["a:h,b:h"], "sex": 0 },
+        { "label": "Males without horns", "alleles": ["a:H","b:H"], "sex": 0 },
+        { "label": "Females with horns", "alleles": ["a:h,b:h"], "sex": 1 },
+        { "label": "Females without horns", "alleles": ["a:H","b:H"], "sex": 1 }
+      ]
+    },
+    /*
+     *  Case 4: Trial 2 - Egg Game III (armor)
+     */
+    {
+      "template": "EggSortGame",
+      "trialGenerator": {
+        "type": "randomize-order",
+        "baseDrake": "a:T,b:T,a:h,b:h,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "drakes": [
+          { alleles: "a:A1,b:A1", sex: 0 },
+          { alleles: "a:A1,b:a", sex: 0 },
+          { alleles: "a:a,b:A1", sex: 0 },
+          { alleles: "a:a,b:a", sex: 0 },
+          { alleles: "a:A1,b:A1", sex: 1 },
+          { alleles: "a:A1,b:a", sex: 1 },
+          { alleles: "a:a,b:A1", sex: 1 },
+          { alleles: "a:a,b:a", sex: 1 }
+        ],
+      },
+      "hiddenAlleles": "t,m,w,h,c,b,fl,hl,d,bog,rh",
+      "baskets": [
+        { "label": "Drakes with no armor plates", "alleles": ["a:a,b:a"] },
+        { "label": "Drakes with three armor plates", "alleles": ["a:A1,b:a", "a:a,b:A1"] },
+        { "label": "Drakes with five armor plates", "alleles": ["a:A1,b:A1"] }
+      ]
+    },
+    /*
+     *  Case 4: Trial 3 - Egg Game III (horns,wings,armor)
+     */
+    {
+      "template": "EggSortGame",
+      "trialGenerator": {
+        "type": "randomize-order",
+        "baseDrake": "a:T,b:T,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "drakes": [
+          { alleles: "a:h,b:h,a:w,b:w,a:a,b:a" },
+          { alleles: "a:h,b:h,a:w,b:w,a:a,b:a" },
+          { alleles: "a:H,b:h,a:W,b:w,a:a,b:a" },
+          { alleles: "a:H,b:H,a:w,b:w,a:A1,b:a" },
+          { alleles: "a:H,b:h,a:w,b:w,a:A1,b:A1" },
+          { alleles: "a:H,b:H,a:W,b:W,a:a,b:a" },
+          { alleles: "a:h,b:H,a:w,b:w,a:A1,b:A1" },
+          { alleles: "a:H,b:H,a:w,b:W,a:a,b:a" }
+        ],
+      },
+      "hiddenAlleles": "t,m,c,b,fl,hl,d,bog,rh",
+      "baskets": [
+        { "label": "Drakes with horns", "alleles": ["a:h,b:h"] },
+        { "label": "Drakes with wings", "alleles": ["a:W", "b:W"] },
+        { "label": "Drakes with armor", "alleles": ["a:A1","b:A1"] }
       ]
     }
   ]
