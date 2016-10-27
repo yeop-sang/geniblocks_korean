6302730e758a43916cf74f0767aec4e9e000cfaa
Kirk Swenson <kswenson@concord.org>
2016-10-26 18:33:30 -0700

    add another genome challenge so Case 1 sequence is: - playground - match single drake (visible user drake) - match single drake (hidden user drake) - match three drakes (hidden user drake)

--- src/resources/authoring/gv2.js
+++ src/resources/authoring/gv2.js
@@ -24,6 +24,20 @@ window.GV2Authoring = [
     },
     {
       "template": "GenomeChallenge",
+      "instructions": "Match the target drake!",
+      "showUserDrake": false,
+      "initialDrake": {
+        "alleles": "a:T,b:T,a:h,b:h,a:A1,b:A1,a:C,b:C,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "sex": 1
+      },
+      "targetDrakes": [{
+        "alleles": "a:w,a:m,b:m,a:fl,a:hl,a:T,b:T,a:h,b:h,a:C,b:C,a:A1,b:A1,a:B,b:B,a:D,b:D,a:rh,b:rh,a:Bog,b:Bog",
+        "sex": 1
+      }],
+      "hiddenAlleles": "t,tk,h,c,a,b,d,bog,rh"
+    },
+    {
+      "template": "GenomeChallenge",
       "instructions": "Match 3 drakes!",
       "trialGenerator": {
         "type": "all-combinations",
