7c4b76c94b11607b95c6013acc6136b40965da43
Sam Fentress <sfentress@concord.org>
2017-01-10 13:24:13 -0500

    Fix modal-dialog test, and fix bug exposed by it.
    We shouldn't hide the drake on receiving ITS message

--- src/code/reducers/user-drake-hidden.js
+++ src/code/reducers/user-drake-hidden.js
@@ -7,6 +7,13 @@ export default function userDrakeHidden(state = initialState, action) {
     // Right now the drake is always hidden unless we are displaying a message
     case actionTypes.MODAL_DIALOG_SHOWN:
       return false;
+    // The following actions don't change the state:
+    case actionTypes.GUIDE_CONNECTED:
+    case actionTypes.GUIDE_ERRORED:
+    case actionTypes.GUIDE_MESSAGE_RECEIVED:
+    case actionTypes.GUIDE_ALERT_RECEIVED:
+      return state;
+    // All other actions re-hide the drake
     default:
       return true;
   }
