753658b8e8202368ccd320b60a47d4640ec2c9fd
Sam Fentress <sfentress@concord.org>
2017-01-23 17:27:18 -0500

    Test loading authored state in EggGame, GenomeChallenge
    These test that, given an authoring object, if we navigate to a
    challenge the proper state will be loaded. In particular, the correct
    drakes will be created in the drakes array.

--- test/templates/egg-game.js
+++ test/templates/egg-game.js
@@ -1,6 +1,21 @@
 import EggGame from '../../src/code/templates/egg-game';
 import GeneticsUtils from '../../src/code/utilities/genetics-utils';
+import reducer from '../../src/code/reducers/';
+import { navigateToChallenge } from '../../src/code/actions';
 import expect from 'expect';
+
+const basicUnderdefinedInitialState = (template, _case, challenge, challenges) => ({
+  template,
+  case: _case,
+  challenge,
+  challenges,
+  challengeType: undefined,
+  interactionType: undefined,
+  goalMoves: null,
+  instructions: undefined,
+  showUserDrake: false,
+  trialSuccess: false
+});
 
 describe('authoredDrakesToDrakeArray()', () => {
   it('Egg Game I should create valid parent drakes', () => {
@@ -50,3 +65,87 @@ describe('authoredDrakesToDrakeArray()', () => {
     }
   });
 });
+
+
+describe('loading authored state into template', () => {
+  describe('in the EggGame II template', () => {
+    let authoring = GeneticsUtils.convertDashAllelesObjectToABAlleles([
+          [
+            {},
+            {
+              "template": "EggGame",
+              "challengeType": "match-target",
+              "mother":{
+                "alleles": "W-W, T-T",
+                "sex": 1
+              },
+              "father": {
+                "alleles": "w-w, T-T",
+                "sex": 0
+              },
+              "targetDrakes": [{},{},{}]
+            }
+          ]
+        ], ["alleles"]);
+
+    let defaultState = reducer(undefined, {});
+    let initialState = defaultState.merge({
+      case: 0,
+      challenge: 0,
+      authoring: authoring
+    });
+
+    let nextState = reducer(initialState, navigateToChallenge(0, 1));
+
+    it('should create the correct drake and trial state on initial load', () => {
+
+      expect(nextState).toEqual(initialState
+        .merge(basicUnderdefinedInitialState("EggGame", 0,1,2))
+        .merge({
+          "challengeType": "match-target",
+          "drakes": [
+            {
+              "alleleString": nextState.drakes[0].alleleString, // we check valid alleles below
+              "sex": 1
+            },
+            {
+              "alleleString": nextState.drakes[1].alleleString,
+              "sex": 0
+            },
+            null,
+            {
+              "alleleString": nextState.drakes[3].alleleString,
+              "sex": nextState.drakes[3].sex
+            },
+            {
+              "alleleString": nextState.drakes[4].alleleString,
+              "sex": nextState.drakes[4].sex
+            },
+            {
+              "alleleString": nextState.drakes[5].alleleString,
+              "sex": nextState.drakes[5].sex
+            }
+          ],
+          "gametes": [ {}, {} ],
+          "goalMoves": nextState.goalMoves,
+          "trials": [ {}, {}, {} ]
+        }));
+    });
+
+    it('should create drakes of the authored genotype', () => {
+
+      expect(GeneticsUtils.isValidAlleleString(nextState.drakes[0].alleleString)).toBe(true);
+      expect(nextState.drakes[0].alleleString.indexOf("a:W,b:W") > -1).toBe(true);
+
+      expect(GeneticsUtils.isValidAlleleString(nextState.drakes[1].alleleString)).toBe(true);
+      expect(nextState.drakes[1].alleleString.indexOf("a:w,b:w") > -1).toBe(true);
+
+      // all trial drakes must be T-T
+      expect(GeneticsUtils.isValidAlleleString(nextState.drakes[3].alleleString)).toBe(true);
+      expect(nextState.drakes[3].alleleString.indexOf("a:T,b:T") > -1).toBe(true);
+      expect(nextState.drakes[4].alleleString.indexOf("a:T,b:T") > -1).toBe(true);
+      expect(nextState.drakes[5].alleleString.indexOf("a:T,b:T") > -1).toBe(true);
+    });
+  });
+});
+
