3454ca642001c6507a5fce162f551bf044dfc97c
Kirk Swenson <kswenson@concord.org>
2017-03-16 14:21:58 -0700

    ITS improvements - Queue ITS messages sent before ITS connection established - Send any queued messages once ITS connection established - More consistent console.logging of ITS messages - Dispatch SessionStarted message before any other messages (e.g. default first navigation) - Dismissing feedback dialog in match drake challenge doesn't dismiss ITS message

--- src/code/reducers/notifications.js
+++ src/code/reducers/notifications.js
@@ -1,5 +1,6 @@
 import Immutable from 'seamless-immutable';
 import actionTypes from '../action-types';
+import { GAMETES_RESET } from '../modules/gametes';
 import urlParams from '../utilities/url-params';
 
 const initialState = Immutable([]);
@@ -12,6 +13,9 @@ export default function notifications(state = initialState, action) {
       }
       else
         return state;
+    // actions which don't clear the guide message
+    case GAMETES_RESET:
+      return state;
     // For now, clear all messages every time the user does anything else
     default:
       return initialState;
