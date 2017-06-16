e43314d7790eb683d6395cb52562a36c5860167a
Eli Kosminsky <ekosminsky@concord.org>
2017-06-15 17:54:26 -0700

    Refactoring end level dialog into a venture pad component

---
+++ src/code/fv-components/venture-pad.js
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+
+const VenturePadView = ({title, screen}) => {
+  return (
+    <div className="venture-pad-container">
+      <div className="venture-pad-backdrop"></div>
+      <div className="venture-pad-background"></div>
+      <div className="venture-pad-overlay"></div>
+      <div className="venture-pad-screen">
+        <div className="venture-pad-title">
+          /{title}
+        </div>
+        <div className="venture-pad-flavor-text">
+          {"Geni_retrieve <get> boot_all </get>"}
+          <br/>
+          {"Scale system flex/accessing drake genetics mainframe..."}
+          <br/>
+          {"/buffering data..."}
+        </div>
+        <div className="venture-pad-separator" id="venture-pad-separator-1"></div>
+        { screen }
+      </div>
+    </div>
+  );
+};
+
+VenturePadView.propTypes = {
+  title: PropTypes.string,
+  screen: PropTypes.object
+};
+
+export default VenturePadView;
