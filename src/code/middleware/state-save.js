0fbac252ae5893c9911220b302116f0e2f9621c2
Eli Kosminsky <ekosminsky@concord.org>
2017-06-26 15:06:35 -0700

    Adding stateVersion number and checking all actions for gem updates

--- src/code/middleware/state-save.js
+++ src/code/middleware/state-save.js
@@ -1,18 +1,21 @@
-import actionTypes from '../action-types';
 import { getClassId, getUserId } from '../utilities/url-params';
 
 export const authoringVersionNumber = 1;
 
-export default () => store => next => action => {
-  let result = next(action);
+const stateVersionNumber = 1;
 
-  if (action.type === actionTypes.CHALLENGE_COMPLETED) {
+export default () => store => next => action => {
+  let prevState = store.getState(),
+      result = next(action),
+      nextState = store.getState();
+
+  // Store updated gems if they have changed
+  if (JSON.stringify(prevState.gems) !== JSON.stringify(nextState.gems)) {
       let userQueryString = getUserQueryString();
 
       if (userQueryString) {
-        let nextState = store.getState(),
-            gems = nextState.gems,
-            stateUpdate = {state: gems};
+        let gems = nextState.gems,
+            stateUpdate = {state: gems, stateVersion: stateVersionNumber};
           
         firebase.database().ref(userQueryString).update(stateUpdate); //eslint-disable-line
       }
