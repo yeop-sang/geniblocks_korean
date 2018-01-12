d1f2f9bf68188b0ab0b1074f3562ab8a7313836d
Chris Hart <chart@concord.org>
2018-01-12 16:41:32 -0500

    More pieces for upcoming JWT implementation

--- src/code/utilities/firebase-auth.js
+++ src/code/utilities/firebase-auth.js
@@ -13,29 +13,41 @@ export default function initFirebase() {
   };
   firebase.initializeApp(config);
 
-  // send request to portal for JWT
-  let jwtUrl = urlParams.domain + "api/v1/jwt/firebase?firebase_app=" + config.projectId;
-
-  let jwtRequest = new Request(jwtUrl);
-  console.log(jwtRequest);
-  fetch(jwtRequest).then(function (response) {
-    return response;
-  }).then(function (response) {
-    if (!response.ok) console.log("nope");
-    else {
-      response.json().then(function (token) {
-        firebase.auth().signInWithCustomToken(token).catch(function (error) {
-          // Handle Errors here.
-          console.log(error);
-          // var errorCode = error.code;
-          // var errorMessage = error.message;
-          // ...
+  // communicate with portal for JWT
+  // if there is no domain parameter, there is no authentication
+  if (!urlParams.domain) {
+    console.log("Not authenticated via portal");
+    return;
+  } else {
+    // send request to portal - for staging:
+    // https://learn.staging.concord.org/api/v1/jwt/firebase?firebase_app=GVStaging
+    let jwtUrl = urlParams.domain + "api/v1/jwt/firebase?firebase_app=" + config.projectId;
+    fetch(jwtUrl, {
+      method: 'post',
+      headers: {
+        'Authorization': `Bearer ${urlParams.token}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'A=1&B=2'
+    }).then(function (response) {
+      return response;
+    }).then(function (response) {
+      if (!response.ok) console.log("nope");
+      else {
+        response.json().then(function (token) {
+          // TODO: This will need some attention once tokens are returned
+          firebase.auth().signInWithCustomToken(token).catch(function (error) {
+            // Handle Errors here.
+            console.log(error);
+            // var errorCode = error.code;
+            // var errorMessage = error.message;
+            // ...
+          });
         });
-      });
-    }
+      }
 
-    console.log("Response: " + response.body);
-  });
-  // https://learn.staging.concord.org/api/v1/jwt/firebase?firebase_app=GVStaging
-  console.log(firebase);
+      console.log("Response: ", response.body);
+    });
+
+  }
 }
