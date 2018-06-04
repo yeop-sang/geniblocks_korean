3b3eaefb4ac58a18846e7ca8e8f3c7430e7195bd
Chris Hart <chart@concord.org>
2018-06-04 10:12:53 -0400

    Add a button to the UI to trigger popups

--- src/code/templates/zoom-challenge.js
+++ src/code/templates/zoom-challenge.js
@@ -10,6 +10,7 @@ export default class ZoomChallenge extends Component {
     var iframeElement = document.getElementById("iframe");
     phone = new iframePhone.ParentEndpoint(iframeElement);
     phone.addListener('challengeWin', this.props.onWinZoomChallenge);
+    phone.addListener('activityLoaded', () => { console.log("loaded!");});
   }
 
   componentWillUnmount() {
@@ -18,11 +19,30 @@ export default class ZoomChallenge extends Component {
 
   render() {
     let url = this.props.zoomUrl;
+    let tempStyle = {
+      width: '30px',
+      height: '30px',
+      position: 'fixed',
+      bottom: '30px',
+      backgroundColor: 'gold',
+      borderRadius: '18px',
+      textAlign: 'center',
+      lineHeight: '1.5em',
+      color: 'black',
+      fontWeight: 'bold',
+      border: '1px solid orange',
+      zIndex: 20,
+      cursor: 'pointer'
+    };
     if (window.location.href.indexOf('/branch/staging') > -1 ) url = this.props.zoomUrl + "&staging=true";
-
+    function togglePopups() {
+      console.log('toggle');
+      phone.post('togglePopups');
+    }
     return (
       <div id="zoom-challenge-container">
-        <iframe id="iframe" src={ url } />
+        <iframe id="iframe" src={url} />
+        <div id="toggle-popups" style={tempStyle} onClick={togglePopups}>?</div>
       </div>
     );
   }
