c642b13a3407d6bdce14d4cdc711bb2e5dc33032
Sam Fentress <sfentress@concord.org>
2018-10-04 16:32:51 -0400

    Add ITS concept score logging

--- src/code/middleware/gv-log.js
+++ src/code/middleware/gv-log.js
@@ -1,7 +1,10 @@
+/* globals firebase */
+
 import actionTypes from '../action-types';
 import templates from '../templates';
 import urlParams from '../utilities/url-params';
 import AuthoringUtils from '../utilities/authoring-utils';
+import { getUserQueryString } from './state-save';
 
 function isLocalHostUrl() {
   const host = window.location.hostname;
@@ -11,6 +14,10 @@ function isLocalHostUrl() {
 const logManagerUrl  = '//cc-log-manager.herokuapp.com/api/logs',
       // disable logging during development unless explicitly enabled
       isLoggingEnabled = !isLocalHostUrl() || (urlParams.log === "true");
+
+
+let userQueryString = getUserQueryString();
+let listeningToITSData = false;
 
 const actionsToExclude = [
   actionTypes.SOCKET_CONNECTED,
@@ -34,6 +41,31 @@ export default loggingMetadata => store => next => action => {
       postToLogManager(message);
     console.log(`%c action`, `color: #03A9F4`, message);
   }
+
+  // listen to ITS updates and log them
+  if (!listeningToITSData && isLoggingEnabled) {
+    if (!userQueryString) userQueryString = getUserQueryString();
+    console.log(userQueryString);
+    if (userQueryString) {
+      listeningToITSData = true;
+      firebase.database().ref(userQueryString + "/itsData").on('value', function(snapshot) {
+        const itsData = snapshot.val();
+        const message =
+          {
+            application: loggingMetadata.applicationName,
+            username: loggingMetadata.userName,
+            run_remote_endpoint: loggingMetadata.returnUrl,
+            session: session,
+            time: Date.now(),
+            activity: AuthoringUtils.getChallengeId(nextState.authoring, nextState.routeSpec),
+            event: "ITS Data Updated",
+            parameters: itsData
+          };
+        postToLogManager(message);
+      });
+    }
+  }
+
   return result;
 };
 
