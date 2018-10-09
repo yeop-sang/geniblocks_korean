22ddee59faa0638b7f90b7777cd66e6127cac32b
Sam Fentress <sfentress@concord.org>
2018-10-09 10:44:49 -0400

    Fix current activity in ITS event logging

--- src/code/middleware/gv-log.js
+++ src/code/middleware/gv-log.js
@@ -11,6 +11,26 @@ function isLocalHostUrl() {
   return (host.indexOf('localhost') >= 0) || (host.indexOf('127.0.0.1') >= 0);
 }
 
+let currentActivity = "";
+
+function logITSData(loggingMetadata) {
+  return function (snapshot) {
+    const itsData = snapshot.val();
+    const message =
+      {
+        application: loggingMetadata.applicationName,
+        username: loggingMetadata.userName,
+        run_remote_endpoint: loggingMetadata.returnUrl,
+        session: session,
+        time: Date.now(),
+        activity: currentActivity,
+        event: "ITS Data Updated",
+        parameters: itsData
+      };
+    postToLogManager(message);
+  };
+}
+
 const logManagerUrl  = '//cc-log-manager.herokuapp.com/api/logs',
       // disable logging during development unless explicitly enabled
       isLoggingEnabled = !isLocalHostUrl() || (urlParams.log === "true");
@@ -45,24 +65,9 @@ export default loggingMetadata => store => next => action => {
   // listen to ITS updates and log them
   if (!listeningToITSData && isLoggingEnabled) {
     if (!userQueryString) userQueryString = getUserQueryString();
-    console.log(userQueryString);
     if (userQueryString) {
       listeningToITSData = true;
-      firebase.database().ref(userQueryString + "/itsData").on('value', function(snapshot) {
-        const itsData = snapshot.val();
-        const message =
-          {
-            application: loggingMetadata.applicationName,
-            username: loggingMetadata.userName,
-            run_remote_endpoint: loggingMetadata.returnUrl,
-            session: session,
-            time: Date.now(),
-            activity: AuthoringUtils.getChallengeId(nextState.authoring, nextState.routeSpec),
-            event: "ITS Data Updated",
-            parameters: itsData
-          };
-        postToLogManager(message);
-      });
+      firebase.database().ref(userQueryString + "/itsData").on('value', logITSData(loggingMetadata));
     }
   }
 
@@ -78,8 +83,8 @@ function getValue(obj, path) {
 
 function createLogEntry(loggingMetadata, action, nextState){
   const eventName = action.type,
-        routeSpec = nextState.routeSpec,
-        activity  = AuthoringUtils.getChallengeId(nextState.authoring, routeSpec);
+        routeSpec = nextState.routeSpec;
+  currentActivity  = AuthoringUtils.getChallengeId(nextState.authoring, routeSpec);
   let parameters = { ...action };
 
   if (action.meta && action.meta.logNextState) {
@@ -113,7 +118,7 @@ function createLogEntry(loggingMetadata, action, nextState){
       run_remote_endpoint: loggingMetadata.returnUrl,
       session: session,
       time: Date.now(),
-      activity: activity,
+      activity: currentActivity,
       event: eventName,
       parameters: parameters
     };
